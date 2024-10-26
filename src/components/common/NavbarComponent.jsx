39d22ee8b40e110391c0172881c4e40f22017644
divya-avuti <avutd01@pfw.edu>
2024-10-26 16:53:27 -0400

    added styling for navbar

--- src/components/common/NavbarComponent.jsx
+++ src/components/common/NavbarComponent.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap';
+import logo from "./../../assets/logo.png";
+import { FaSearch } from 'react-icons/fa';
+import { RiUserHeartFill } from "react-icons/ri";
+import { BiSolidSelectMultiple } from "react-icons/bi";
+import './../../styles/style.css';
 
 function NavbarComponent() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
-      <Container>
-        {/* Logo */}
-        <Navbar.Brand href="/" className="font-weight-bold">
-          <i className="fas fa-store fa-lg mr-2"></i> MyMarket
+      <Container fluid>
+        {/* Logo and Brand Name */}
+        <Navbar.Brand href="/" className="font-weight-bold d-flex flex-column align-items-center">
+          <img 
+            src={logo}
+            alt="Campus Mart Logo" 
+            className="navbar-brand-logo"
+          />
         </Navbar.Brand>
 
-        {/* Search bar (centered) */}
-        <Form inline="true" className="mx-auto w-50">
-          <FormControl
-            type="text"
-            placeholder="Search products..."
-            className="mr-2"
-            style={{ borderRadius: "20px" }}
-          />
-          <Button variant="outline-light">
-            <i className="fas fa-search"></i>
+        <Navbar.Toggle aria-controls="navbarSearchAndLinks" />
+        <Navbar.Collapse id="navbarSearchAndLinks" className="justify-content-between">
+          {/* Simple Search Bar */}
+          <Form className="d-flex mx-auto search-bar-container">
+            <FormControl
+              type="text"
+              placeholder="Search products..."
+              className="search-input"
+            />
+           <Button variant="light" className="search-button">
+            <FaSearch />
           </Button>
-        </Form>
+          </Form>
 
-        {/* Profile and Choose Role Links */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="/profile" className="d-flex align-items-center mx-2">
-              <i className="fas fa-user-circle fa-lg mr-1"></i> Profile
+          {/* Profile and Choose Role Links */}
+          <Nav>
+            <Nav.Link href="/profile" className="d-flex flex-column align-items-center mx-2">
+              <RiUserHeartFill className="nav-link-icon" />
+              <span className="nav-link-text">Profile</span>
             </Nav.Link>
-            <Nav.Link href="/choose-role" className="d-flex align-items-center mx-2">
-              <i className="fas fa-user-tag fa-lg mr-1"></i> Choose Role
+            <Nav.Link href="/choose-role" className="d-flex flex-column align-items-center mx-2">
+              <BiSolidSelectMultiple className="nav-link-icon" />
+              <span className="nav-link-text">Choose Role</span>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
